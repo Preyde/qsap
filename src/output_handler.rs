21b782124fbae280a83c5d218fa6a6772c2fce30
Pascal Frank <pascalfrank17@gmail.com>
2021-11-14 14:09:55 +0100

    added to handle the output of config

--- src/output_handler.rs
+++ src/output_handler.rs
@@ -1,34 +1,15 @@
-use sap_adt_bindings::config::{
-    freestyle_config::{FreeStyleConfig, FreeStyleResponse},
-    AdtError, AdtResponse, Config, Responses,
-};
+use sap_adt_bindings::config::{AdtError, Responses};
 
 pub mod output_handler {}
 
-// pub trait Handler<C, T, E>
-// where
-//     C: Config<T, E>,
-//     T: AdtResponse<Responses>,
-//     E: AdtError,
-// {
-//     fn handle(response: Responses);
-// }
+pub fn handle_output(response: Responses) {
+    match response {
+        Responses::Program(v) => println!("{}", v),
+        Responses::Class(v) => println!("{}", v),
+        Responses::FreeStyle(t) => t.display(),
+    }
+}
 
-pub struct OutputHandler {}
-
-// impl OutputHandler {
-//     fn handle<T, E>(response: Result<T, E>)
-//     where
-//         T: AdtResponse<Responses> + Sized,
-//         E: AdtError,
-//     {
-//     }
-// }
-// impl Handler<FreeStyleResponse> for OutputHandler {
-//     fn handle(res: FreeStyleResponse)
-//     where
-//         T: Config + Sized,
-//     {
-//         res.get_table();
-//     }
-// }
+pub fn handle_error(error: AdtError) {
+    println!("{}", error);
+}
